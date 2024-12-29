8ccd7d0472a32a68c6ff8e9e66184f6c62b1bc4f
SamGostling <samuelgostling@hotmail.co.uk>
2024-12-29 14:01:22 +0000

    Add unit tests

---
+++ src/raytracer/internal/image_test.go
@@ -0,0 +1,45 @@
+package internal
+
+import (
+	"image/color"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewImage(t *testing.T) {
+	width, height := 256, 192
+	img := NewImage(width, height)
+	assert.Equal(t, width, img.Width, "Width should be equal")
+	assert.Equal(t, height, img.Height, "Height should be equal")
+	assert.NotNil(t, img.Img, "Image should not be nil")
+}
+
+func TestPixelTransform(t *testing.T) {
+	width, height := 2, 2
+	img := NewImage(width, height)
+	transform := func(x, y int) color.RGBA {
+		return color.RGBA{uint8(x * 255), uint8(y * 255), 0, 255}
+	}
+	img.PixelTransform(transform)
+	assert.Equal(t, color.RGBA{0, 0, 0, 255}, img.Img.At(0, 0), "Pixel (0,0) should be black")
+	assert.Equal(t, color.RGBA{255, 0, 0, 255}, img.Img.At(1, 0), "Pixel (1,0) should be red")
+	assert.Equal(t, color.RGBA{0, 255, 0, 255}, img.Img.At(0, 1), "Pixel (0,1) should be green")
+	assert.Equal(t, color.RGBA{255, 255, 0, 255}, img.Img.At(1, 1), "Pixel (1,1) should be yellow")
+}
+
+func TestSave(t *testing.T) {
+	width, height := 2, 2
+	img := NewImage(width, height)
+	transform := func(x, y int) color.RGBA {
+		return color.RGBA{uint8(x * 255), uint8(y * 255), 0, 255}
+	}
+	img.PixelTransform(transform)
+	filename := "./test_image.png"
+	img.Save(filename)
+	defer os.Remove(filename)
+
+	_, err := os.Stat(filename)
+	assert.False(t, os.IsNotExist(err), "File should exist")
+}
---
+++ src/raytracer/internal/vector_test.go
@@ -0,0 +1,44 @@
+package internal
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestVectorLength(t *testing.T) {
+	v := Vector{3, 4, 0}
+	expected := 5.0
+	result := v.Length()
+	assert.Equal(t, expected, result, "they should be equal")
+}
+
+func TestVectorDot(t *testing.T) {
+	v1 := Vector{1, 2, 3}
+	v2 := Vector{4, 5, 6}
+	expected := 32.0
+	result := v1.Dot(v2)
+	assert.Equal(t, expected, result, "they should be equal")
+}
+
+func TestVectorNormalize(t *testing.T) {
+	v := Vector{3, 4, 0}
+	expected := Vector{0.6, 0.8, 0}
+	result := v.Normalize()
+	assert.Equal(t, expected, result, "they should be equal")
+}
+
+func TestVectorMultiplyScalar(t *testing.T) {
+	v := Vector{1, 2, 3}
+	scalar := 2.0
+	expected := Vector{2, 4, 6}
+	result := v.MultiplyScalar(scalar)
+	assert.Equal(t, expected, result, "they should be equal")
+}
+
+func TestVectorAddScalar(t *testing.T) {
+	v := Vector{1, 2, 3}
+	scalar := 2.0
+	expected := Vector{3, 4, 5}
+	result := v.AddScalar(scalar)
+	assert.Equal(t, expected, result, "they should be equal")
+}
